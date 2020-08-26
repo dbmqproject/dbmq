0dfb524b4973a744ddc8ffa11b3350b4b0c711f3
lnxpy <lnxpylnxpy@gmail.com>
2020-08-27 00:35:53 +0430

    exception handler added + main function created

--- setup.py
+++ setup.py
@@ -5,7 +5,20 @@ For more information about setup file, check out
 https://docs...
 """
 
+import docker
 import webserver
+from extras import exceptions, succeeds
 
-# working tree is clean..
-# TODO: Managing the containers.
+
+def main(client):
+    # TODO: Managing the containers
+    pass
+
+
+if __name__ == '__main__':
+    try:
+        client = docker.from_env()
+        print(succeeds.DOCKER_EXCEPTION)
+        main(client)
+    except docker.errors.DockerException:
+        print(exceptions.DOCKER_EXCEPTION)
