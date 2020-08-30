7c141913e132ef7fa2260b96ea692bfd44662a88
lnxpy <lnxpylnxpy@gmail.com>
2020-08-30 15:21:37 +0430

    using Failure class instead of small components

--- extras/exceptions.py
+++ extras/exceptions.py
@@ -4,7 +4,12 @@ Exception text handler.
 For more information about exceptions file, check out
 https://docs...
 """
-from .textstyle import Alert, Style
+from .textstyle import Failure
 
-DOCKER_EXCEPTION = '%sDBMQ Could Not Connect to Docker!%s' % (
-    Alert.failed, Style.clear)
+
+DOCKER_EXCEPTION = Failure('DBMQ Could Not Connect to Docker!',
+                           [
+                               'Make sure Docker service is running properly.',
+                               'Use an un-staff user.',
+                               'Either run up with the root user or, make sure your user is a member of Docker group.'
+                           ], 'https://docs...').text()
