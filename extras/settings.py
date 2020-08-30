f4eab0d2169964215964a54709863fcd08324fd1
lnxpy <lnxpylnxpy@gmail.com>
2020-08-30 15:21:48 +0430

    PEP8 observed

--- extras/settings.py
+++ extras/settings.py
@@ -6,37 +6,41 @@ For more information about settings file, check out
 https://docs...
 """
 
+
 class Image:
     """
     Available images are stored here.
     Example script: `settings.Images.ubuntu`
     """
-    
+
     ubuntu = 'ubuntu'
     centos = 'centos'
+
 
 class Server:
     """
     Available web servers are stored here.
     Example script: `settings.Server.django`"""
-    
+
     django = 'django'
     flask = 'flask'
+
 
 class Database:
     """
     Available databases are stored here.
     Example script: `settings.Database.postgres`
     """
-    
+
     postgres = 'postgres'
     mysql = 'mysql'
+
 
 class Broker:
     """
     Available brokers are stored here.
     Example script: `settings.Broker.rabbitmq`
     """
-    
+
     rabbitmq = 'rabbitmq'
     redis = 'redis'
