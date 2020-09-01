0565d7be1a59436a13654ce2be22f44e00cd2828
lnxpy <lnxpylnxpy@gmail.com>
2020-09-01 23:20:01 +0430

    exception turned to (independent) exception + some additional exceptions added

--- extras/exceptions.py
+++ extras/exceptions.py
@@ -4,12 +4,32 @@ Exception text handler.
 For more information about exceptions file, check out
 https://docs...
 """
-from .textstyle import Failure
+from .textstyle import Failure, Succeed
 
 
-DOCKER_EXCEPTION = Failure('DBMQ Could Not Connect to Docker!',
-                           [
-                               'Make sure Docker service is running properly.',
-                               'Use an un-staff user.',
-                               'Either run up with the root user or, make sure your user is a member of Docker group.'
-                           ], 'https://docs...').text()
+DOCKER_EXCEPTION_FAILED = Failure('DBMQ Could Not Connect to Docker!',
+                                  [
+                                      'Make sure Docker service is running properly.',
+                                      'Use an un-staff user.',
+                                      'Either run up with the root user or, make sure your user is a member of Docker group.'
+                                  ], 'https://docs...').text()
+
+DOCKER_EXCEPTION_SUCCESS = Succeed('DBMQ Connected to Docker!',
+                                   [
+                                       'Reading the configurations..',
+                                       'Checking the images existence..',
+                                       'Building your images based on following local configs...'
+                                   ]).text()
+
+IMAGE_BUILT = Succeed('Your Core Image Built Successfully.',
+                      [
+                          'Your core webserver image has been created.',
+                          'All required packages are stored in your image.',
+                          'Your image is being runned in a minute..'
+                      ]).text()
+
+CONNECTION_REFUSED = Failure('The Connection Between DBMQ and Docker Server Refused!',
+                             [
+                                 'Please check your connection and try again.',
+                                 'Boot up the Docker service with no latency or duration.'
+                             ], 'https://docs...').text())
