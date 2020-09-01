5dc58138684c5141c012597d05866176a2f9848c
lnxpy <lnxpylnxpy@gmail.com>
2020-09-01 23:20:21 +0430

    success positive exception file removed

--- extras/succeeds.py
+++
@@ -1,9 +0,0 @@
-"""
-Succeed text handler.
-
-For more information about succeeds file, check out
-https://docs...
-"""
-from .textstyle import Succeed
-
-DOCKER_EXCEPTION = Succeed('DBMQ Connected to Docker!').text()
