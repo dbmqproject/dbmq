abc47df0dc3b167633ed55cbd0f13c318d7085de
lnxpy <lnxpylnxpy@gmail.com>
2020-09-06 17:03:51 +0430

    nocache field feature added + root_configs beautified

--- setup.py
+++ setup.py
@@ -21,27 +21,31 @@ def main(client):
         beautified_configs = json.dumps(configs, sort_keys=True, indent=3)
 
     # Main Docker configurations are stored in root_config
-    root_config = {
+    root_configs = {
         'path': path.join(configs['DOCKER_FILES_DIR'],
                           webserver.SERVER_CONFIGS['CONTAINER']['IMAGE']),
         'tag': webserver.SERVER_CONFIGS['CONTAINER']['NAME'],
         'buildargs': {
             'PROJECT_NAME': webserver.SERVER_CONFIGS['NAME'],
         },
+        'nocache': webserver.SERVER_CONFIGS['CONTAINER']['NOCACHE'],
     }
+
+    beautified_root_configs = json.dumps(
+        root_configs, sort_keys=True, indent=3)
     print(beautified_configs)
 
     # Building process starts
     try:
-        client.images.build(**root_config)
+        client.images.build(**root_configs)
         print(exceptions.IMAGE_BUILT)
+        print(beautified_root_configs)
     except:
         print(exceptions.CONNECTION_REFUSED)
         return
 
     # TODO: Running the images (with exceptional conditions)
     # TODO: Ending up the program "Press any key to exit.."-like
-    # TODO: ++ Adding cache-usage feature in webserver configs
 
 
 if __name__ == '__main__':
