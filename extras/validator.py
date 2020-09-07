be06bbc3e4ea315a75a172eea2d512a32b87c6e9
lnxpy <lnxpylnxpy@gmail.com>
2020-09-07 17:06:29 +0430

    config validator file created

---
+++ extras/validator.py
@@ -0,0 +1,16 @@
+"""
+Validator.
+
+For more information about validator file, check out
+https://docs...
+"""
+
+# TODO: Reading from /webserver.py & start validating the data
+
+"""
+First checking if all necessary fields are filled out.
+Then, start filling out optional fields.
+
+Finally, returning cleaned root_configs to the main source (setup.py) and
+continue processing.
+"""
