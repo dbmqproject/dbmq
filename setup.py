a70df6e1e6071fe3e7696cfe9f378e2672d3837f
lnxpy <lnxpylnxpy@gmail.com>
2020-09-11 02:01:08 +0430

    root config is now based on validator data + build function separated with loadingbar

--- setup.py
+++ setup.py
@@ -8,53 +8,62 @@ https://docs...
 import docker
 import webserver
 import json
+import sys
 from os import path
 from extras import exceptions
 from extras.validator import ServerConfigsValidator
 
 
 def main(client):
-    # TODO: Start managing containers
-
     # Reading local_configs for the paths
-    with open('local_configs.json', 'r') as f:
+    with open('workdir.json', 'r') as f:
         configs = json.load(f)
-        beautified_configs = json.dumps(configs, sort_keys=True, indent=3)
+        beautified_configs = json.dumps(
+            configs, sort_keys=True, indent=3)
 
     process = ServerConfigsValidator(webserver.SERVER_CONFIGS)
 
     if process['status']:
+        main_data = process['data']
         # Main Docker configurations are stored in root_config
         root_configs = {
             'path': path.join(configs['DOCKER_FILES_DIR'],
-                              webserver.SERVER_CONFIGS['CONTAINER']['IMAGE']),
-            'tag': webserver.SERVER_CONFIGS['CONTAINER']['NAME'],
+                              main_data['CONTAINER']['IMAGE']),
+            'tag': main_data['CONTAINER']['NAME'],
             'buildargs': {
-                'PROJECT_NAME': webserver.SERVER_CONFIGS['NAME'],
+                'PROJECT_NAME': main_data['NAME'],
             },
-            'nocache': webserver.SERVER_CONFIGS['CONTAINER']['NOCACHE'],
+            'nocache': main_data['CONTAINER']['NOCACHE'],
         }
-
-        beautified_process = json.dumps(process, sort_keys=True, indent=3)
+        beautified_root_configs = json.dumps(process, sort_keys=True, indent=3)
     else:
         print(process['data'])
         return
 
     print(beautified_configs)
-    print(beautified_process)
+    print(beautified_root_configs)
 
     # Building process starts
-    try:
-        client.images.build(**root_configs)
-        print(exceptions.IMAGE_BUILT)
-        print(beautified_root_configs)
-    except:
-        print(exceptions.CONNECTION_REFUSED)
-        return
+    build(root_configs)
 
-    # TODO: Ending up the program "Press any key to exit.."-like
-    # TODO: Using process['data'] as the main reference instead of webserver configs (because of the conflicts
-    # in BaseModel pydantic
+
+def build(root_configs):
+    toolbar_width = 30
+
+    # setup toolbar
+    sys.stdout.write("Building Your Core Image.. [%s]" % (" " * toolbar_width))
+    sys.stdout.flush()
+    # return to start of line, after '['
+    sys.stdout.write("\b" * (toolbar_width+1))
+
+    for i in range(toolbar_width):
+        client.images.build(**root_configs)
+        # update the bar
+        sys.stdout.write('-')
+        sys.stdout.flush()
+
+    sys.stdout.write("]\n")  # this ends the progress bar
+    print(exceptions.IMAGE_BUILT)
 
 
 if __name__ == '__main__':
