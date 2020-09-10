d7b63c28bc7d9e83b5ea3e112c1318fb8dcb5805
lnxpy <lnxpylnxpy@gmail.com>
2020-09-10 19:24:48 +0430

    validation conditions added

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ import webserver
 import json
 from os import path
 from extras import exceptions
+from extras.validator import ServerConfigsValidator
 
 
 def main(client):
@@ -20,32 +21,40 @@ def main(client):
         configs = json.load(f)
         beautified_configs = json.dumps(configs, sort_keys=True, indent=3)
 
-    # Main Docker configurations are stored in root_config
-    root_configs = {
-        'path': path.join(configs['DOCKER_FILES_DIR'],
-                          webserver.SERVER_CONFIGS['CONTAINER']['IMAGE']),
-        'tag': webserver.SERVER_CONFIGS['CONTAINER']['NAME'],
-        'buildargs': {
-            'PROJECT_NAME': webserver.SERVER_CONFIGS['NAME'],
-        },
-        'nocache': webserver.SERVER_CONFIGS['CONTAINER']['NOCACHE'],
-    }
+    process = ServerConfigsValidator(webserver.SERVER_CONFIGS)
 
-    beautified_root_configs = json.dumps(
-        root_configs, sort_keys=True, indent=3)
-    print(beautified_configs)
+    if process['status']:
+        # Main Docker configurations are stored in root_config
+        root_configs = {
+            'path': path.join(configs['DOCKER_FILES_DIR'],
+                              webserver.SERVER_CONFIGS['CONTAINER']['IMAGE']),
+            'tag': webserver.SERVER_CONFIGS['CONTAINER']['NAME'],
+            'buildargs': {
+                'PROJECT_NAME': webserver.SERVER_CONFIGS['NAME'],
+            },
+            'nocache': webserver.SERVER_CONFIGS['CONTAINER']['NOCACHE'],
+        }
 
-    # Building process starts
-    try:
-        client.images.build(**root_configs)
-        print(exceptions.IMAGE_BUILT)
-        print(beautified_root_configs)
-    except:
-        print(exceptions.CONNECTION_REFUSED)
+        beautified_process = json.dumps(process, sort_keys=True, indent=3)
+    else:
+        print(process['data'])
         return
 
-    # TODO: Running the images (with exceptional conditions)
+    print(beautified_configs)
+    print(beautified_process)
+
+    # Building process starts
+    # try:
+    #    client.images.build(**root_configs)
+    #    print(exceptions.IMAGE_BUILT)
+    #    print(beautified_root_configs)
+    # except:
+    #    print(exceptions.CONNECTION_REFUSED)
+    #    return
+
     # TODO: Ending up the program "Press any key to exit.."-like
+    # TODO: Using process['data'] as the main reference instead of webserver configs (because of the conflicts
+    # in BaseModel pydantic
 
 
 if __name__ == '__main__':
