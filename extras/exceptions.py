9ee017716a8ef75a2c1acd2f5048da894cee507c
lnxpy <lnxpylnxpy@gmail.com>
2020-09-13 00:41:01 +0430

    BUILDING_IMAGE object created with Notification class

--- extras/exceptions.py
+++ extras/exceptions.py
@@ -4,7 +4,7 @@ Exception text handler.
 For more information about exceptions file, check out
 https://docs...
 """
-from .textstyle import Failure, Succeed
+from .textstyle import Failure, Succeed, Notification
 
 
 DOCKER_EXCEPTION_FAILED = Failure('DBMQ Could Not Connect to Docker!',
@@ -18,13 +18,18 @@ DOCKER_EXCEPTION_SUCCESS = Succeed('DBMQ Connected to Docker!',
                                    [
                                        'Reading the configurations..',
                                        'Checking the images existence..',
-                                       'Building your images based on following local configs...'
                                    ]).text()
 
-IMAGE_BUILT = Succeed('Your Core Image Built Successfully.').text()
+IMAGE_BUILT = Succeed('Core image has been built successfully.').text()
 
 CONNECTION_REFUSED = Failure('The Connection Between DBMQ and Docker Server Refused!',
                              [
                                  'Please check your connection and try again.',
                                  'Boot up the Docker service with no latency or duration.'
                              ], 'https://docs...').text()
+
+BUILDING_IMAGE = Notification([
+    'Sending the arguments to Dockerfile..',
+    'Environment variables are setting..',
+    'Building the core image and installing the packages.. (it might take a few minutes)',
+]).text()
