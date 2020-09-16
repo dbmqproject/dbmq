8795fa1b881ba7e3c8d89cf4c3c81435bbe15163
lnxpy <lnxpylnxpy@gmail.com>
2020-09-17 02:52:28 +0430

    PEP8 observed

--- extras/exceptions.py
+++ extras/exceptions.py
@@ -7,36 +7,55 @@ https://docs...
 from .textstyle import Failure, Succeed, Notification
 
 
-DOCKER_EXCEPTION_FAILED = Failure('DBMQ Could Not Connect to Docker!',
-                                  [
-                                      'Make sure Docker service is running properly.',
-                                      'Use an un-staff user.',
-                                      'Either run up with the root user or, make sure your user is a member of Docker group.'
-                                  ], 'https://docs...').text()
+DOCKER_EXCEPTION_FAILED = Failure(
+    'DBMQ Could Not Connect to Docker!',
+    [
+        'Make sure Docker service is running properly.',
+        'Use an un-staff user.',
+        'Either run up with the root user or, make sure your user is a member of Docker group.'
+    ], 'https://docs...'
+).text()
 
-DOCKER_EXCEPTION_SUCCESS = Succeed('DBMQ Connected to Docker!',
-                                   [
-                                       'Reading the configurations..',
-                                       'Checking the images existence..',
-                                   ]).text()
+DOCKER_EXCEPTION_SUCCESS = Succeed(
+    'DBMQ Connected to Docker!',
+    [
+        'Reading the configurations..',
+        'Checking the images existence..',
+    ]
+).text()
 
 IMAGE_BUILT = Succeed('Core image has been built successfully.').text()
 
-CONNECTION_REFUSED = Failure('The Connection Between DBMQ and Docker Server Refused!',
-                             [
-                                 'Please check your connection and try again.',
-                                 'Boot up the Docker service with no latency or duration.'
-                             ], 'https://docs...').text()
+CONNECTION_REFUSED = Failure(
+    'The Connection Between DBMQ and Docker Server Refused!',
+    [
+        'Please check your connection and try again.',
+        'Boot up the Docker service with no latency or duration.'
+    ], 'https://docs...'
+).text()
 
-BUILDING_IMAGE = Notification([
-    'Sending the arguments to Dockerfile..',
-    'Environment variables are setting..',
-    'Building the core image and installing the packages.. (It might take a few minutes)',
-]).text()
+BUILDING_IMAGE = Notification(
+    [
+        'Sending the arguments to Dockerfile..',
+        'Setting the environment variables..',
+        'Building the core image and installing the packages.. (It might take a few minutes)',
+    ]
+).text()
 
-RUNNING_CONTAINER = Notification([
-    'Running your containers..'
-]).text()
+RUNNING_CONTAINER = Notification(
+    [
+        'Running your containers..'
+    ]
+).text()
 
 CONTAINER_IS_RUNNING = Succeed(
-    'Core container is running in the background.').text()
+    'Core container is running in the background.'
+).text()
+
+CONTAINER_FAILED_IN_RUNNING = Failure(
+    'Could Not Run the Core Image',
+    [
+        'Make sure if there is any conflict with tags and solve them.',
+        'Check your Docker stability.'
+    ]
+).text()
