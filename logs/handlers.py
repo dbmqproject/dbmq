06035eff24d725342e5694cf619cb074d641359d
lnxpy <lnxpylnxpy@gmail.com>
2020-10-12 23:45:36 +0330

    handler uses ini file

--- logs/handlers.py
+++ logs/handlers.py
@@ -6,44 +6,16 @@ https://docs...
 """
 
 import logging
+import logging.config
+import pathlib
 
 
-# custom stream handler
-class MyStreamHandler(logging.StreamHandler):
-    def emit(self, record):
-        if record.levelno == self.level:
-            super().emit(record)
-
-
-def logger_handler():
+def create_logger(base):
     # base logging configuration
-    logger = logging.getLogger('DBMQ')
-    logger.setLevel(logging.DEBUG)
-
-    # stream handler
-    stream_handler = MyStreamHandler()
-    stream_handler.setLevel(logging.INFO)
-
-    # file handler
-    file_handler = logging.FileHandler(
-        './data/file.log',
-        mode='a',
-        encoding='UTF-8',
+    logging.config.fileConfig(
+        fname=base / 'logs/logging_config.ini',
+        disable_existing_loggers=False,
     )
-    file_handler.setLevel(logging.WARNING)
 
-    # formatters
-    stream_formatter = logging.Formatter(
-        '%(message)s',
-    )
-    file_formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        datefmt='%d-%b-%y %H:%M:%S',
-    )
-    stream_handler.setFormatter(stream_formatter)
-    file_handler.setFormatter(file_formatter)
-
-    logger.addHandler(stream_handler)
-    logger.addHandler(file_handler)
-
+    logger = logging.getLogger('DBMQ-LOGGER')
     return logger
