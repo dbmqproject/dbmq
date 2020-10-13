373bdb7a388cf1bb2cd9ca514b391f7c785c0e49
lnxpy <lnxpylnxpy@gmail.com>
2020-10-14 01:47:13 +0330

    all objects are not converted to plain text

--- extras/exceptions.py
+++ extras/exceptions.py
@@ -7,64 +7,56 @@ https://docs...
 from .textstyle import Failure, Notification, Succeed
 
 DOCKER_EXCEPTION_FAILED = Failure(
-    'DBMQ Could Not Connect to Docker!',
+    'DBMQ Could Not Connect to Docker.',
     [
         'Make sure Docker service is running properly.',
         'Use an un-staff user.',
         'Either run up with the root user or, make sure your user is a member of Docker group.'
     ], 'https://docs...'
-).text()
+)
 
 DOCKER_EXCEPTION_SUCCESS = Succeed(
-    'DBMQ Connected to Docker!',
+    'DBMQ Connected to Docker.',
     [
         'Reading the configurations..',
         'Checking the images existence..',
     ]
-).text()
+)
 
-IMAGE_BUILT = Succeed('Core Image Has Been Built Successfully.').text()
+IMAGE_BUILT = Succeed('Core Image Has Been Built Successfully.')
 
 CONNECTION_REFUSED = Failure(
-    'The Connection Between DBMQ and Docker Server Refused!',
+    'The Connection Between DBMQ and Docker Server Refused.',
     [
         'Please check your connection and try again.',
         'Boot up the Docker service with no latency or duration.',
         'Make sure you have stored your images and Dockerfiles on DOCKER_FILES_DIR.',
     ], 'https://docs...'
-).text()
+)
 
 BUILDING_IMAGE = Notification(
-    [
-        'Sending the arguments to Dockerfile..',
-        'Setting the environment variables..',
-        'Building the core image and installing the packages.. (It might take a few minutes)',
-    ]
-).text()
+    'Start Interacting with Docker.',
+)
 
 RUNNING_CORE_CONTAINER = Notification(
-    [
-        'Running your core container..'
-    ]
-).text()
+    'Running the Core Container.',
+)
 
 CONTAINER_IS_RUNNING = Succeed(
     'Core Container is Running in the Background.'
-).text()
+)
 
 CONTAINER_FAILED_IN_RUNNING = Failure(
-    'Could Not Run the Core Image',
+    'Could Not Run the Core Image.',
     [
         'Make sure if there is any conflict with tags and solve them.',
         'Check your Docker stability.'
     ]
-).text()
+)
 
 STREAM_LOGGING_STARTED = Notification(
-    [
-        'Stream logger is processing the console..',
-    ]
-).text()
+    'Stream Logger is Activated on the Console.',
+)
 
 EXIT_INTERRUPT = Failure(
     'This Session is About to Die.',
@@ -73,8 +65,16 @@ EXIT_INTERRUPT = Failure(
         'All other services are still available.',
         'View logs and keep developing on your containers manually.'
     ]
-).text()
+)
 
 SOMETHING_IS_WRONG = Failure(
     'Something Went Wrong in the %s Process.'
-).text()
+)
+
+RIDI = Failure(
+    'Haji ridi',
+    [
+        'bad ridi',
+        'khoob ridi'
+    ]
+)
